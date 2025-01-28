9ada28d0510c99e93dc556aa3746363a14fa5307
Brad Koby <kobyb@miamioh.edu>
2025-01-28 08:11:42 -0500

    Fix overwrite deprecation

--- ssm_parameter.tf
+++ ssm_parameter.tf
@@ -24,7 +24,6 @@ resource "aws_ssm_parameter" "miamioh_data_update" {
   value = yamlencode(each.value.initial_data)
 
   description = "Added by terraform aws_ssm_parameter with updates"
-  overwrite   = true
   tags        = local.all_tags
 }
 
